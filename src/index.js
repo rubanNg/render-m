03f2c9e1ddb5a67b282e16152a92363d6c277999
Alexander <a.ruban@amarkets.com>
2022-09-06 20:05:30 +0200

    port

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-const port = 5000;
 app.use(express.static(path.resolve(__dirname, "../static")));
 
 app.get('/', (request, response) => {
-  response.render("index.html", {z: 1});
+  response.render("index.html");
 });
 
-app.listen(port, () => console.log(`Run: http://localhost:${port}`));
+app.listen(process.env.PORT, () => console.log(`Run: http://localhost:${process.env.PORT}`));
