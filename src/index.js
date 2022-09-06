03b49501d76cf7389e8cd9584b7d637e6d49c729
Alexander <a.ruban@amarkets.com>
2022-09-06 20:27:02 +0200

    ax

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path')
+const axios = require('axios');
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, "../static")));
